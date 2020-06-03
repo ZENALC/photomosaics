1b646b3eb9d54661164cf8786ec12274d1e8a252
mshrestha <mihir.xtha@gmail.com>
2020-06-03 13:51:00 -0400

    refactor

--- photomosaics.py
+++ photomosaics.py
@@ -14,7 +14,7 @@ class PhotoMosaic:
         self.image = self.get_image(imageFile, resize=True)
         self.width, self.height = self.image.size
         self.imageDictionary = self.load_images(imagesFolder)
-        self.pixels = list(self.image.getdata())  # get list of RGB values from image
+        self.pixels = list(self.image.getdata())
         self.matrix = self.get_matrix()
         self.editedImage = self.photo_mosaic()
 
@@ -22,34 +22,20 @@ class PhotoMosaic:
         """Returns a 2D list of RGB tuples of image"""
         return [[self.pixels[self.width * y + x] for x in range(self.width)] for y in range(self.height)]
 
-    @staticmethod
-    def get_average(flatList: list) -> tuple:
-        """Return average RGB tuple from a list of given RGB values"""
-        r, g, b = 0, 0, 0
-        length = len(flatList)
-        for elem in flatList:
-            r += elem[0]
-            g += elem[1]
-            b += elem[2]
-        r = r / length
-        g = g / length
-        b = b / length
-        return r, g, b
-
-    def get_image(self, path: str, dimension: tuple = None, squareImage: bool = False, resize: bool = False) -> Image:
+    def get_image(self, path: str, thumbnail: tuple = None, squareImage: bool = False, resize: bool = False) -> Image:
         """Return an Image object"""
         with Image.open(path) as image:
-            if squareImage:
+            if squareImage:  # if we need to get a square image
                 width, height = image.size
                 if width != height:  # check if image is square
                     toCrop = min((width, height))  # get minimum of width or height
                     image = self.crop_center(image, toCrop, toCrop)  # get cropped, square image
 
-            if resize:
+            if resize:  # if we need to resize the image, resize it to baseWidth
                 image = self.resize_image(image)
 
-            if dimension:  # if some dimension is given, then resize it
-                image.thumbnail(dimension)
+            if thumbnail:  # if some thumbnail dimension is given, then resize it
+                image.thumbnail(thumbnail)
 
             if image.mode == 'P':  # if transparent, convert to RGBA
                 image = image.convert('RGBA')
@@ -57,15 +43,6 @@ class PhotoMosaic:
                 image = image.convert('RGB')
 
             return image
-
-    @staticmethod
-    def crop_center(pil_img: Image, crop_width: float, crop_height: float) -> Image:
-        """Return a center-cropped image - copied from Pillow documentation"""
-        img_width, img_height = pil_img.size
-        return pil_img.crop(((img_width - crop_width) // 2,
-                             (img_height - crop_height) // 2,
-                             (img_width + crop_width) // 2,
-                             (img_height + crop_height) // 2))
 
     def resize_image(self, image: Image) -> Image:
         """Resize image to a bigger size so sub-images are more visible"""
@@ -85,7 +62,7 @@ class PhotoMosaic:
         for file in os.listdir():
             if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
                 continue
-            image = self.get_image(file, dimension=dimension, squareImage=True)
+            image = self.get_image(file, thumbnail=dimension, squareImage=True)
             pixels = list(image.getdata())
             average = self.get_average(pixels)
             if average not in imagesDictionary:
@@ -95,14 +72,6 @@ class PhotoMosaic:
 
         os.chdir(previous_path)
         return imagesDictionary
-
-    @staticmethod
-    def euclidean_distance(l1: tuple, l2: tuple) -> float:
-        """Return euclidean distance between two RGB tuples"""
-        r = (l1[0] - l2[0]) ** 2
-        g = (l1[1] - l2[1]) ** 2
-        b = (l1[2] - l2[2]) ** 2
-        return math.sqrt(r + g + b)
 
     def best_match(self, rgbTuple: tuple) -> Image:
         """Return best possible image from imageDict that matches rgbTuple based on euclidean distance"""
@@ -160,6 +129,37 @@ class PhotoMosaic:
         os.chdir(previous_path)
 
     @staticmethod
+    def euclidean_distance(l1: tuple, l2: tuple) -> float:
+        """Return euclidean distance between two RGB tuples"""
+        r = (l1[0] - l2[0]) ** 2
+        g = (l1[1] - l2[1]) ** 2
+        b = (l1[2] - l2[2]) ** 2
+        return math.sqrt(r + g + b)
+
+    @staticmethod
+    def get_average(flatList: list) -> tuple:
+        """Return average RGB tuple from a list of given RGB values"""
+        r, g, b = 0, 0, 0
+        length = len(flatList)
+        for elem in flatList:
+            r += elem[0]
+            g += elem[1]
+            b += elem[2]
+        r = r / length
+        g = g / length
+        b = b / length
+        return r, g, b
+
+    @staticmethod
+    def crop_center(pil_img: Image, crop_width: float, crop_height: float) -> Image:
+        """Return a center-cropped image - copied from Pillow documentation"""
+        img_width, img_height = pil_img.size
+        return pil_img.crop(((img_width - crop_width) // 2,
+                             (img_height - crop_height) // 2,
+                             (img_width + crop_width) // 2,
+                             (img_height + crop_height) // 2))
+
+    @staticmethod
     def progress_bar(y: int, height: int):
         """Display a progress bar when rendering mosaic"""
         percentage = math.ceil(y / height * 100)
@@ -169,5 +169,5 @@ class PhotoMosaic:
 
 
 if __name__ == "__main__":
-    a = PhotoMosaic('monkey.jpg', 'Random Images', targetWidth=15000, step=200)
+    a = PhotoMosaic('monkey.jpg', 'Random Images', targetWidth=5000, step=50)
     a.save_image()
