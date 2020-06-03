50c74050cea5fd8379464f8d4fecc5d204c68fda
mshrestha <mihir.xtha@gmail.com>
2020-06-03 11:27:15 -0400

    converted to a class

--- photomosaics.py
+++ photomosaics.py
@@ -5,172 +5,169 @@ import random
 import math
 
 
-# Return average RGB tuple from a list of given RGB values
-def get_average(flatList: list) -> tuple:
-    r, g, b = 0, 0, 0
-    length = len(flatList)
-    for elem in flatList:
-        r += elem[0]
-        g += elem[1]
-        b += elem[2]
-    r = r / length
-    g = g / length
-    b = b / length
-    return r, g, b
+class PhotoMosaic:
+    def __init__(self, imageFile, imagesFolder, step=100, targetWidth=12500):
+        self.folder = imagesFolder
+        self.step = step
+        self.targetWidth = targetWidth
+        self.imageFile = imageFile
+        self.image = self.get_image(imageFile, resize=True)
+        self.width, self.height = self.image.size
+        self.imageDictionary = self.load_images(imagesFolder)
+        self.pixels = list(self.image.getdata())  # get list of RGB values from image
+        self.matrix = self.get_matrix()
+        self.editedImage = self.photo_mosaic()
 
+    def get_matrix(self):
+        """Returns a 2D list of RGB tuples of image"""
+        return [[self.pixels[self.width * y + x] for x in range(self.width)] for y in range(self.height)]
 
-# Return an Image object
-def get_image(path: str, dimension: tuple = None, resize: bool = False) -> Image:
-    with Image.open(path) as image:
-        if resize:
-            width, height = image.size
-            if width != height:  # check if image is square
-                toCrop = min((width, height))  # get minimum of width or height
-                image = crop_center(image, toCrop, toCrop)  # get cropped, square image
+    @staticmethod
+    def get_average(flatList: list) -> tuple:
+        """Return average RGB tuple from a list of given RGB values"""
+        r, g, b = 0, 0, 0
+        length = len(flatList)
+        for elem in flatList:
+            r += elem[0]
+            g += elem[1]
+            b += elem[2]
+        r = r / length
+        g = g / length
+        b = b / length
+        return r, g, b
 
-        if dimension:  # if some dimension is given, then resize it
-            image.thumbnail(dimension)
+    def get_image(self, path: str, dimension: tuple = None, squareImage: bool = False, resize: bool = False) -> Image:
+        """Return an Image object"""
+        with Image.open(path) as image:
+            if squareImage:
+                width, height = image.size
+                if width != height:  # check if image is square
+                    toCrop = min((width, height))  # get minimum of width or height
+                    image = self.crop_center(image, toCrop, toCrop)  # get cropped, square image
 
-        if image.mode == 'P':  # if transparent, convert to RGBA
-            image = image.convert('RGBA')
-        else:  # else force convert to RGB
-            image = image.convert('RGB')
+            if resize:
+                image = self.resize_image(image)
 
-        return image
+            if dimension:  # if some dimension is given, then resize it
+                image.thumbnail(dimension)
 
+            if image.mode == 'P':  # if transparent, convert to RGBA
+                image = image.convert('RGBA')
+            else:  # else force convert to RGB
+                image = image.convert('RGB')
 
-# Return a center-cropped image - copied from Pillow documentation
-def crop_center(pil_img: Image, crop_width: float, crop_height: float) -> Image:
-    img_width, img_height = pil_img.size
-    return pil_img.crop(((img_width - crop_width) // 2,
-                         (img_height - crop_height) // 2,
-                         (img_width + crop_width) // 2,
-                         (img_height + crop_height) // 2))
+            return image
 
+    @staticmethod
+    def crop_center(pil_img: Image, crop_width: float, crop_height: float) -> Image:
+        """Return a center-cropped image - copied from Pillow documentation"""
+        img_width, img_height = pil_img.size
+        return pil_img.crop(((img_width - crop_width) // 2,
+                             (img_height - crop_height) // 2,
+                             (img_width + crop_width) // 2,
+                             (img_height + crop_height) // 2))
 
-# Resize image to a bigger size so more sub-pixels are visible
-def resize_image(image: Image, targetWidth: int = 2500) -> Image:
-    width, height = image.size
-    baseWidth = targetWidth
-    widthPercent = baseWidth / width
-    newHeight = int(height * widthPercent)
-    return image.resize((baseWidth, newHeight), Image.ANTIALIAS)
-
-
-# Load all images in given path and return dictionary containing them
-def load_images(path: str, dimension: tuple = None) -> dict:
-    print("Loading images...")
-    imagesDictionary = {}
-    previous_path = os.getcwd()
-    os.chdir(path)
-
-    for file in os.listdir():
-        if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
-            continue
-        image = get_image(file, dimension=dimension, resize=True)
-        pixels = list(image.getdata())
-        average = get_average(pixels)
-        if average not in imagesDictionary:
-            imagesDictionary[average] = [image]
-        else:
-            imagesDictionary[average].append(image)
-
-    os.chdir(previous_path)
-    return imagesDictionary
-
-
-# Return euclidean distance between two RGB tuples
-def euclidean_distance(l1: tuple, l2: tuple) -> float:
-    r = (l1[0] - l2[0]) ** 2
-    g = (l1[1] - l2[1]) ** 2
-    b = (l1[2] - l2[2]) ** 2
-    return math.sqrt(r + g + b)
-
-
-# Return best possible image from imageDict that matches rgbTuple based on euclidean distance
-def best_match(rgbTuple: tuple, imageDict: dict) -> Image:
-    bestKeys = []
-    bestDistance = None
-    for otherTuple in imageDict.keys():
-        currentDistance = euclidean_distance(rgbTuple, otherTuple)
-        if not bestDistance or currentDistance < bestDistance:
-            bestDistance = currentDistance
-            bestKeys.insert(0, otherTuple)
-    key = bestKeys[0]  # not sure how to guarantee that there is no repeats
-    return random.choice(imageDict[key])  # return a random image if there is more than one image with associated key
-
-
-# Return a manipulated image with mosaic implemented
-def photo_mosaic(imagePath: str, imageDict: dict, step: int, targetWidth: int = 2500) -> Image:
-    print("Creating a mosaic...")
-    image = get_image(imagePath)  # load main image to make photo mosaic
-    width, height = image.size  # get width and height
-    if width < targetWidth:  # enlarge image
-        image = resize_image(image, targetWidth=targetWidth)
+    def resize_image(self, image: Image) -> Image:
+        """Resize image to a bigger size so sub-images are more visible"""
         width, height = image.size
-    pixels = list(image.getdata())  # get list of RGB values from image
-    matrix = [[pixels[width * y + x] for x in range(width)] for y in range(height)]  # get 2D array of RGB values
+        baseWidth = self.targetWidth
+        widthPercent = baseWidth / width
+        newHeight = int(height * widthPercent)
+        return image.resize((baseWidth, newHeight), Image.ANTIALIAS)
 
-    editedImage = Image.new(image.mode, (width, height))  # instantiate new image
-    for y in range(0, height, step):  # loop over rows incrementing by step
-        y2 = y + step if y + step < height else height  # make end point y + step if not over height, else height
-        for x in range(0, width, step):  # loop over columns until exhaustion
-            x2 = x + step if x + step < width else width  # make end point x + step if not over width, else width
-            subList = []  # instantiate new list to append subMatrix values to
-            for z in range(y, y2):  # loop over subMatrix
-                subMatrix = matrix[z][x:x2]
-                subList.append(subMatrix)
-            flat_list = [rgbTuple for elem in subList for rgbTuple in elem]  # flatten list
-            average = get_average(flat_list)  # get average RGB tuple
-            img = best_match(average, imageDict)  # find best matching image
-            editedImage.paste(img, (x, y))  # paste image to x, y coordinate
-        progress_bar(y, height)
-    print()  # print statement because of progress bar not creating new line
-    return editedImage
+    def load_images(self, path: str, dimension: tuple = None) -> dict:
+        """Load all images in given path and return dictionary containing them"""
+        print("Loading images...")
+        imagesDictionary = {}
+        previous_path = os.getcwd()
+        os.chdir(path)
 
+        for file in os.listdir():
+            if not file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                continue
+            image = self.get_image(file, dimension=dimension, squareImage=True)
+            pixels = list(image.getdata())
+            average = self.get_average(pixels)
+            if average not in imagesDictionary:
+                imagesDictionary[average] = [image]
+            else:
+                imagesDictionary[average].append(image)
 
-# Save image to a folder
-def save_image(image: Image, imageFile: str):
-    folderName = "Photo Mosaics"
-    if not os.path.exists(folderName):
-        os.mkdir(folderName)
+        os.chdir(previous_path)
+        return imagesDictionary
 
-    previous_path = os.getcwd()
-    os.chdir(folderName)
+    @staticmethod
+    def euclidean_distance(l1: tuple, l2: tuple) -> float:
+        """Return euclidean distance between two RGB tuples"""
+        r = (l1[0] - l2[0]) ** 2
+        g = (l1[1] - l2[1]) ** 2
+        b = (l1[2] - l2[2]) ** 2
+        return math.sqrt(r + g + b)
 
-    name, ext = imageFile.split('.')
-    counter = 0
+    def best_match(self, rgbTuple: tuple) -> Image:
+        """Return best possible image from imageDict that matches rgbTuple based on euclidean distance"""
+        bestKeys = []
+        bestDistance = None
+        for otherTuple in self.imageDictionary.keys():
+            currentDistance = self.euclidean_distance(rgbTuple, otherTuple)
+            if not bestDistance or currentDistance < bestDistance:
+                bestDistance = currentDistance
+                bestKeys.insert(0, otherTuple)
+        key = bestKeys[0]
+        return random.choice(self.imageDictionary[key])
 
-    output_image = f"{name}-mosaic.{ext}"
-    if os.path.exists(output_image):
-        while os.path.exists(output_image):
-            output_image = f"{name}-mosaic{counter}.{ext}"
-            counter += 1
+    def photo_mosaic(self) -> Image:
+        """Return a manipulated image with mosaic implemented"""
+        print("Creating a mosaic...")
+        editedImage = Image.new(self.image.mode, (self.width, self.height))
+        for y in range(0, self.height, self.step):
+            y2 = y + self.step if y + self.step < self.height else self.height
+            for x in range(0, self.width, self.step):
+                x2 = x + self.step if x + self.step < self.width else self.width
+                subList = []
+                for z in range(y, y2):
+                    subMatrix = self.matrix[z][x:x2]
+                    subList.append(subMatrix)
+                flat_list = [rgbTuple for elem in subList for rgbTuple in elem]
+                average = self.get_average(flat_list)
+                img = self.best_match(average)
+                editedImage.paste(img, (x, y))
+            self.progress_bar(y, self.height)
+        print()
+        return editedImage
 
-    path = os.path.join(os.getcwd(), output_image)
-    image.save(path)
-    print(f"Photo mosaic has been successfully saved to {path}.")
-    os.chdir(previous_path)
+    def save_image(self):
+        """Save image to a folder"""
+        folderName = "Photo Mosaics"
+        if not os.path.exists(folderName):
+            os.mkdir(folderName)
 
+        previous_path = os.getcwd()
+        os.chdir(folderName)
 
-# Display a progress bar when rendering mosaic
-def progress_bar(y: int, height: int):
-    percentage = math.ceil(y / height * 100)
-    sys.stdout.write('\r')
-    sys.stdout.write("[%-20s] %d%% rendered." % ('=' * (percentage // 5), percentage))
-    sys.stdout.flush()
+        name, ext = self.imageFile.split('.')
+        counter = 0
 
+        output_image = f"{name}-mosaic.{ext}"
+        if os.path.exists(output_image):
+            while os.path.exists(output_image):
+                output_image = f"{name}-mosaic{counter}.{ext}"
+                counter += 1
 
-def main():
-    step = 100  # how many pixels we'll jump over, the higher it is, the more HD the sub-image will appear
-    targetWidth = 12500  # how big output image will be
-    folder = 'Black Lives Matter'  # folder to get images from
-    imageDict = load_images(folder, dimension=(step, step))  # load images to paste on
-    imageFile = 'georgefloyd.jpg'  # image we'll be making a photo mosaic out of
-    editedImage = photo_mosaic(imageFile, imageDict=imageDict, step=step, targetWidth=targetWidth)  # get a photo mosaic
-    # editedImage.show()  # view image
-    save_image(editedImage, imageFile)  # save image
+        path = os.path.join(os.getcwd(), output_image)
+        self.editedImage.save(path)
+        print(f"Photo mosaic has been successfully saved to {path}.")
+        os.chdir(previous_path)
+
+    @staticmethod
+    def progress_bar(y: int, height: int):
+        """Display a progress bar when rendering mosaic"""
+        percentage = math.ceil(y / height * 100)
+        sys.stdout.write('\r')
+        sys.stdout.write("[%-20s] %d%% rendered." % ('=' * (percentage // 5), percentage))
+        sys.stdout.flush()
 
 
 if __name__ == "__main__":
-    main()
+    a = PhotoMosaic('monkey.jpg', 'Random Images', targetWidth=15000, step=200)
+    a.save_image()
